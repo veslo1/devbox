06750b2c06447019c2ec794f3ebe6c9822d733ca
ajdrew <ajdrew@outlook.com>
2015-11-19 14:58:43 -0500

    t

--- app.js
+++ app.js
@@ -16,11 +16,13 @@ var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('html', require('hbs').__express);
+app.set('views', __dirname + '/views/html');
 app.set('view engine', 'html');
 
 app.use('/', routes);
