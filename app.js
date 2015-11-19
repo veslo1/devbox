cb4f5bb0058c3ed77dded6b3cd438ba94aae2cc1
ajdrew <ajdrew@outlook.com>
2015-11-19 11:27:55 -0500

    t

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -55,5 +56,12 @@ app.use(function(err, req, res, next) {
   });
 });
 
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
 
 module.exports = app;
