d96bb06691153e51925ae2e0bdbe9f984d9c5fe7
ajdrew <ajdrew@outlook.com>
2015-11-19 10:41:56 -0500

    t

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
