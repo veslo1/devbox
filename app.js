2e533c2cabe2204a6661f573c93e27788e4ed50c
ajdrew <ajdrew@outlook.com>
2015-11-19 21:49:17 -0500

    t

--- app.js
+++ app.js
@@ -9,17 +9,16 @@ var Db = require('mongodb').Db;
 var Server = require('mongodb').Server;
 
 // EXPRESS - CONFIGURATION
-app.configure(function() {
-  app.set('port', process.env.PORT || 80);
-});
+app.listen(80);
 
 // EXPRESS - ROUTES
 app.get('/', function(req, res) {
 res.send('Hello World');
 });
 
-http.createServer(app).listen(app.get('port'), function() {
-  console.log("Express server listening on port " + app.get('port'));
-});
+// http.createServer(app).listen(app.get('port'), function() {
+// console.log("Express server listening on port " + app.get('port'));
+// });
 
 // app.listen(process.env.PORT || 80);
+module.exports = app;
