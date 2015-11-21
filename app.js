01dc46278bb06005f2707c7cd463f8167aa489fc
ajdrew <ajdrew@outlook.com>
2015-11-21 10:50:41 -0500

    t

--- app.js
+++ app.js
@@ -9,15 +9,17 @@ var Server = require('mongodb').Server;
 // EXPRESS - CONFIGURATION
 var app = express();
 app.listen(80);
-app.use(express.static( path.join(__dirname, 'public')));
+// app.use(express.static( path.join(__dirname, 'public')));
 
 // EXPRESS - ROUTES
+var html_dir = './public/';
+
 app.get('/', function(req, res) {
 res.send('Hello World');
 });
 
 app.get('/test', function(req, res) {
-  res.sendFile('index.html');
+  res.sendFile(html_dir + 'index.html');
 });
 
 // SERVER - START
