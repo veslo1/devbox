05a4dcf09da48977205615a154810b9bbd8cb1d7
ajdrew <ajdrew@outlook.com>
2015-11-23 16:34:14 -0500

    t

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var Server = require('mongodb').Server;
 // EXPRESS - CONFIGURATION
 var app = express();
 app.listen(80);
-app.use(express.static( path.join(__dirname, 'public')));
+app.use('/static', express.static(__dirname + '/public'));
 
 // EXPRESS - ROUTES
 var html_dir = '/var/www/devbox/public/';
@@ -20,14 +20,6 @@ res.send('Hello World');
 
 app.get('/test', function(req, res) {
   res.sendFile(html_dir + 'index.html');
-});
-
-app.get('/remark', function(req, res) {
-  res.sendFile(html_dir + '/remark2');
-});
-
-app.get('/remark2', function(req, res) {
-  res.sendFile(html_dir + 'resources/remark/classic/mmenu/html/index.html');
 });
 
 // SERVER - START
